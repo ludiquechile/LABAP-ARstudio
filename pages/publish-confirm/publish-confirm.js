9e700124051a95a1a3bd85e5c7c03712dd779c1d
LDQ <contacto@ludique.cl>
2024-04-15 09:51:11 -0400

    Update publish-confirm.js

--- pages/publish-confirm/publish-confirm.js
+++ pages/publish-confirm/publish-confirm.js
@@ -18,7 +18,7 @@ window.onload = async () => {
         return;
     }
 
-    let response = await fetch(`https://nousar.ludique.xyz/gatekeeper/${queryDict.code}`);
+    let response = await fetch(`https://nousar.ludique.xyz/gatekeeper/authenticate/${queryDict.code}`);
     response = await response.json();
 
     const package = new Package(window.session);
